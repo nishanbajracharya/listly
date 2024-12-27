f9cba9a33de902444e26c63ac87118d08ea500a8
nishanbajracharya <nishanbajracharya@lftechnology.com>
2024-12-27 20:10:31 +0545

    Update ranking test

--- tests/modules/ranking.test.ts
+++ tests/modules/ranking.test.ts
@@ -7,14 +7,8 @@ describe('Ranking tests', () => {
 	let list: string[];
 
 	beforeAll(() => {
-		list = [
-			'A',
-			'B',
-			'C',
-			'D',
-			'E',
-			'F',
-		];
+    // Array from A to Z
+		list = Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i));;
 
 		ranking = new Ranking(list);
 	});
@@ -94,4 +88,32 @@ describe('Ranking tests', () => {
 
 		expect(list).toEqual(expect.arrayContaining(result));
 	});
+
+  it('should run matches using recursive flag', () => {
+    ranking = new Ranking(list);
+		const listLength = list.length;
+
+    const totalCountWithoutRecursive = listLength * (listLength - 1) / 2;
+
+    let matchesPlayed = 0;
+
+		while (ranking.matches.length > 0) {
+			const match = ranking.getMatch();
+
+			const first = match[0];
+			const second = match[1];
+
+			const firstScore = ranking.state[first];
+			const secondScore = ranking.state[second];
+
+			ranking.runMatch(first, second, true);
+
+			expect(ranking.state[first]).to.be.greaterThanOrEqual(firstScore + 1);
+			expect(ranking.state[second]).to.eq(secondScore);
+      
+      matchesPlayed++;
+		}
+    
+    expect(matchesPlayed).to.be.lessThanOrEqual(totalCountWithoutRecursive);
+  });
 });
