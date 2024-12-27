c553612c4b2e0397296cb339f704e4c5b289855c
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2024-12-27 18:53:24 +0545

    Add test in CI

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/listly/',
-  plugins: [react()],
-  test: {
-    environment: 'jsdom'
-  }
+  plugins: [react()]
 });
