4465347bb080b3f9ae0cb683714ab40e72c4afbc
nishanbajracharya <nishanbajracharya@lftechnology.com>
2024-12-27 20:15:23 +0545

    Add coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    environment: 'jsdom'
+    environment: 'jsdom',
+    coverage: {
+      provider: 'istanbul'
+    }
   },
 });
